59d478e7aae6bdd81b41bd8ee364f8d9f336e649
Nathan St.Amand <nathansaint810@gmail.com>
2020-03-27 15:57:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -29,7 +29,7 @@ class Dog
   end 
   
   def self.new_from_db(row)
-    dog = Dog.new(name:row[1], breed:[2], id: row[0])
+    dog = Dog.new(name:row[0], breed:[0], id: row[0])
     dog
   end 
   
