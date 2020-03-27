25cf6b1710a7f48b8bb88d9de9fbb07d005167e5
Nathan St.Amand <nathansaint810@gmail.com>
2020-03-27 16:02:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -33,4 +33,14 @@ class Dog
     dog
   end 
   
+  def self.find_by_name(name)
+    sql = <<-SQL 
+    SELECT * FROM dogs 
+    WHERE name = ?
+    SQL
+    
+    DB[:conn].execute(sql, name).map do |row| 
+      self.new_from_db(row[0])
+  end
+  
 end 
